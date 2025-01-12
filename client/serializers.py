3ff09c1492637205d5d3c17b187f3105cde9de50
badsha-ugv <badsha.ugv>
2025-01-12 13:48:32 +0600

    job updated

--- client/serializers.py
+++ client/serializers.py
@@ -71,4 +71,53 @@ class JobSerializer(serializers.ModelSerializer):
                 # return None
 
         return job
+    def update(self, instance, validated_data):
+        # Update Job instance attributes
+        vacancy_data = validated_data.pop('vacancy', [])
+
+        # for attr, value in validated_data.items():
+        #     setattr(instance, attr, value)
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        # Extract the vacancy data from validated_data
+
+        # Create a list to hold the updated vacancies
+        updated_vacancies = []
+
+        # Loop through the provided vacancy data
+        for vacancy_info in vacancy_data:
+            vacancy_id = vacancy_info.get('id', None)
+
+            if 'user' in vacancy_info and isinstance(vacancy_info['user'], User): 
+                vacancy_info['user'] = vacancy_info['user'].id
+
+            if vacancy_id:
+                # Update the existing vacancy
+                try:
+                    vacancy_instance = instance.vacancy.get(id=vacancy_id)
+                    vacancy_serializer = VacancySerializer(vacancy_instance, data=vacancy_info)
+                    if vacancy_serializer.is_valid():
+                        vacancy_instance = vacancy_serializer.save()
+                        updated_vacancies.append(vacancy_instance)  # Add to updated vacancies list
+                    else:
+                        print(f'Vacancy serializer is not valid for ID {vacancy_id}:', vacancy_serializer.errors)
+                except Vacancy.DoesNotExist:
+                    print(f'Vacancy with ID {vacancy_id} does not exist.')
+            else:
+                # Create a new vacancy if no ID is provided
+                vacancy_serializer = VacancySerializer(data=vacancy_info)
+                if vacancy_serializer.is_valid():
+                    vacancy_instance = vacancy_serializer.save()
+                    updated_vacancies.append(vacancy_instance)  # Add the new vacancy to the list
+                else:
+                    print('Vacancy serializer is not valid for new vacancy:', vacancy_serializer.errors)
+
+        # Use set() to update the many-to-many relationship
+        instance.vacancy.set(updated_vacancies)
+
+        # Save the updated job instance
+        instance.save()
+        return instance
+    
 
