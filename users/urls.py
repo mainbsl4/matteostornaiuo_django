200c30fad025e31b63467a86a33902e1ed4248d4
mainbsl4 <mainbsl4@gmail.com>
2025-01-16 20:13:56 +0600

    main

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import StaffSignupAPIView, ClientSignupAPIView
+from .views import StaffSignupAPIView, ClientSignupAPIView, StaffInvitationList
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -11,5 +11,6 @@ urlpatterns = [
     path("clientsignup/", ClientSignupAPIView.as_view(), name="signup"),
     path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),\
+    path("staffinvitation/", StaffInvitationList.as_view(), name="staff_invitation"),\
 ]
