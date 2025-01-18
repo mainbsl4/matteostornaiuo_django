cb8f58d7b729bdb0f3f269b16e3d91a6e9aaf313
badsha-ugv <badsha.ugv>
2025-01-18 14:44:00 +0600

    staff invitation send and notification send

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 
 from unfold.admin import ModelAdmin
 
-from .models import FavouriteStaff
+from .models import FavouriteStaff, Notification
 
 
 @admin.register(FavouriteStaff)
 class FavouriteStaffAdmin(ModelAdmin):
-    pass 
+    list_display = ('company','staff','staff__id') 
+
+    # show number of staff
+    
+    
+
+@admin.register(Notification)
+class NotificationAdmin(ModelAdmin):
+    list_display = ('user', 'created_at', 'is_read')
+    list_editable = ('is_read', )
