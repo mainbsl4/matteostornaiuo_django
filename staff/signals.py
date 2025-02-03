6b4450beeacdb6c390896e640eefa28ae522069e
badsha-ugv <badsha.ugv>
2025-02-03 16:56:43 +0600

    cord error

--- staff/signals.py
+++ staff/signals.py
@@ -10,7 +10,10 @@ def add_staff_to_mystaff(sender, instance, created, **kwargs):
     if created:
         invitation = Invitation.objects.filter(staff_email=instance.user.email).first()
         if invitation:
+            instance.is_letme_staff = False
+            instance.save()
             invited_by = invitation.staff_invitation.user
-            client = CompanyProfile.objects.filter(user=invited_by).first()
+            # user = CompanyProfile.objects.filter(user=invited_by).first()
+            client = CompanyProfile.objects.get(user=invited_by)
             MyStaff.objects.create(staff=instance, client=client, status=True)
             # send email to invited staff
--- staff/urls.py
+++ staff/urls.py
@@ -5,6 +5,8 @@ urlpatterns =[
     path('staff/profile/', views.StaffProfileView.as_view()),
     path('staff/profile/<int:pk>/', views.StaffProfileView.as_view()),
     
+    path('myshift/', views.StaffShiftView.as_view()),
+  
     path('staff/shift/', views.ShiftRequestView.as_view()), # all shift
     path('staff/shift/<int:pk>/', views.ShiftRequestView.as_view()), # shift details
 
@@ -12,6 +14,9 @@ urlpatterns =[
     path('staff/shift/checkin/', views.ShiftCheckinView.as_view()), # checkin details
     path('staff/shift/<int:pk>/checkout/', views.ShiftCheckoutView.as_view()), # checkin details
     path('staff/shift/checkout/', views.ShiftCheckoutView.as_view()), # checkin details
+
+    path('action/job/application/', views.JobApplicationView.as_view()),
+    path('action/job/application/<int:pk>/', views.JobApplicationView.as_view()),
 
 ]
 
