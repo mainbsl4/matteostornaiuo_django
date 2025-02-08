c95225f1acc49828f99e2806d2a8561c39f7878e
badsha-ugv <badsha.ugv>
2025-02-08 17:14:56 +0600

    do base command for status update

--- users/models.py
+++ users/models.py
@@ -32,9 +32,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Skill(models.Model):
     name = models.CharField(max_length=200)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return self.name
 
