e85a639aa0d8267c7d6cdbe13e91b96f2c6f9315
badsha-ugv <badsha.ugv>
2025-02-22 12:36:29 +0600

    job role price included

--- dashboard/models.py
+++ dashboard/models.py
@@ -8,9 +8,6 @@ from staff.models import Staff
 from client.models import CompanyProfile, Vacancy
 
 
-
-
-    
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # to whom
     message = models.CharField(max_length=255)
@@ -24,14 +21,3 @@ class Notification(models.Model):
     
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name} - {self.message[:20]}...'
-
-class CompanyReview(models.Model):
-    profile = models.ForeignKey(CompanyProfile, on_delete=models.CASCADE)
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE, blank=True)
-    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, blank=True)
-    rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    review = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f'{self.staff.user} - Review On {self.vacancy.job_title} '
