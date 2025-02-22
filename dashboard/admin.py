d514b3ee4f1517a4e458f90bb0e8a687336739ba
badsha-ugv <badsha.ugv>
2025-02-22 12:36:29 +0600

    job role price included

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,14 +2,10 @@ from django.contrib import admin
 
 from unfold.admin import ModelAdmin
 
-from .models import  Notification, CompanyReview
+from .models import  Notification
 
 
 @admin.register(Notification)
 class NotificationAdmin(ModelAdmin):
     list_display = ('user', 'created_at', 'is_read', 'message')
     list_editable = ('is_read', )
-
-@admin.register(CompanyReview)
-class CompanyReviewAdmin(ModelAdmin):
-    list_display = ('profile', 'staff', 'vacancy', 'rating', 'created_at')
