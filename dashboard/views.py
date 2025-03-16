6e31cba0c55f569e13bf4ff5f367371e59df7f56
badsha-ugv <badsha.ugv>
2025-03-16 10:50:30 +0600

    fix job view error

--- dashboard/views.py
+++ dashboard/views.py
@@ -128,7 +128,7 @@ class FeedJobView(APIView):
                     "id": vacancy.id,
                     "job_status": vacancy.job_status,
                     "job_title": vacancy.job.title,
-                    "company_logo": vacancy.job.company.company_logo or None,
+                    "company_logo": vacancy.job.company.company_logo.url or None,
                     "number_of_staff": vacancy.number_of_staff,
                     "start_date": vacancy.open_date,
                     "start_time": vacancy.start_time,
