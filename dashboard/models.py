71425638f31e23dd32095cb6c0de5b55a7bdab3d
badsha-ugv <badsha.ugv@gmail.com>
2025-04-02 10:22:01 +0600

    new model added

--- dashboard/models.py
+++ dashboard/models.py
@@ -21,3 +21,45 @@ class Notification(models.Model):
     
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name} - {self.message[:20]}...'
+
+class Report(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+    message = models.TextField()
+    attachment = models.FileField(upload_to='reports/', blank=True, null=True)
+    is_resolved = models.BooleanField(default=False)
+    
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.user} report for {self.type}'
+    class Meta:
+        verbose_name_plural = 'Reports'
+        ordering = ['-created_at']
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name_plural = 'FAQs'
+        ordering = ['-created_at']
+
+
+class TermsAndConditions(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    document = models.FileField(upload_to='terms_and_conditions/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Terms and Conditions'
+        ordering = ['-created_at']
